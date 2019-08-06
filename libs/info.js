ebb10bc0d6fb6afeb271ec7e7425ecf06fe13168
AcaDosic <labyrinthman@gmail.com>
2019-08-06 14:58:57 +0200

    js and html divided

--- libs/info.js
+++ libs/info.js
@@ -1,5 +1,4 @@
-$(function() 
-{
+$(function() {
 	 $("#infoBox")
 	.css( 
 	{
@@ -8,8 +7,7 @@ $(function()
 	.dialog({ autoOpen: false, 
 		show: { effect: 'fade', duration: 500 },
 		hide: { effect: 'fade', duration: 500 } 
-	});
-	
+	});	
 	 $("#infoButton")
        .text("") // sets text to empty
 	.css(
@@ -17,49 +15,38 @@ $(function()
 	  "background":"rgba(0,0,0,0)", "opacity":"0.9", 
 	  "position":"absolute", "top":"8px", "left":"14px"
 	}) // adds CSS
-    .append("<img width='32' height='32' src='../assets/textures/icon-info.png'/>")
+    .append("<img width='32' height='32' src='assets/textures/icinf.png'/>")
     .button()
 	.click( 
 		function() 
 		{ 
 			$("#infoBox").dialog("open");
 		});
-
-
 	 $("#color")
 	.css({"z-index":"2", "background":"rgba(0,0,0,0)", "opacity":"0.9", "position":"absolute", "top":"8px", "right":"14px"})
-	.append("<img width='32' id='red' height='32' src='../assets/textures/icon-red.png'/>")
-    .append("<img width='32' id='green' height='32' src='../assets/textures/icon-green.png'/>")
-    .append("<img width='32' id='blue' height='32' src='../assets/textures/icon-blue.png'/>")
-
+	.append("<img width='32' id='red' height='32' src='assets/textures/icon-red.png'/>")
+    .append("<img width='32' id='green' height='32' src='assets/textures/icon-green.png'/>")
+    .append("<img width='32' id='blue' height='32' src='assets/textures/icon-blue.png'/>")
 	    $("#red").click(function(){
-
 	    	selMaterial = 'floor06';
-	    	floatDomino.material.map = new THREE.TextureLoader().load('../assets/textures/' + selMaterial + '.png');
+	    	floatDomino.material.map = new THREE.TextureLoader().load('assets/textures/' + selMaterial + '.png');
 	    	floatDomino.material.map.wrapS = floatDomino.material.map.wrapT = THREE.RepeatWrapping; 
 				floatDomino.material.map.repeat.set( 1, 8 );
 				floatDomino.material.needsUpdate = true;
-
 	    });
-
-	    $("#green").click(function(){
-	    	
+	    $("#green").click(function(){	    	
 	    	selMaterial = 'floor_06';
-	    	floatDomino.material.map = new THREE.TextureLoader().load('../assets/textures/' + selMaterial + '.png');
+	    	floatDomino.material.map = new THREE.TextureLoader().load('assets/textures/' + selMaterial + '.png');
 	    	floatDomino.material.map.wrapS = floatDomino.material.map.wrapT = THREE.RepeatWrapping; 
 				floatDomino.material.map.repeat.set( 1, 8 );
 				floatDomino.material.needsUpdate = true;
-
 	    });
-
-	    $("#blue").click(function(){
-	    	
+	    $("#blue").click(function(){	    	
 	    	selMaterial = 'floor15';
-	    	floatDomino.material.map = new THREE.TextureLoader().load('../assets/textures/' + selMaterial + '.png');
+	    	floatDomino.material.map = new THREE.TextureLoader().load('assets/textures/' + selMaterial + '.png');
 	    	floatDomino.material.map.wrapS = floatDomino.material.map.wrapT = THREE.RepeatWrapping; 
 				floatDomino.material.map.repeat.set( 1, 8 );
 				floatDomino.material.needsUpdate = true;
-
 	    });
 
 });
